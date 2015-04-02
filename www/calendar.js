ee7ec67911d84e5fd3c5d37af7e93a702618915d
optimixmobile <opti-mix@hotmail.fr>
2015-04-02 15:25:09 +0200

    Update calendar.js

--- www/calendar.js
+++ www/calendar.js
@@ -3,7 +3,7 @@ var calendar = {
 		cordova.exec(
 			successCallback, // success callback function
 			errorCallback, // error callback function
-			'calendar', // mapped to our native Java class called "CalendarPlugin"
+			'Calendar', // mapped to our native Java class called "CalendarPlugin"
 			'addCalendarEntry', // with this action name
 			[
 				{ // and this array of custom arguments to create our entry
